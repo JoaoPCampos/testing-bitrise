34ba8f4731cabc2e944a72d567a595dd85c883d5
joCs <jonybfc@gmail.com>
2024-12-04 16:46:18 +0000

    Adding ryuk again

--- TestCordin/TestCordin/Coordinator.swift
+++ TestCordin/TestCordin/Coordinator.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import RyukNetwork
 
 protocol Coordinator: ObservableObject {
 
@@ -108,7 +109,7 @@ struct FirstView: View {
 
 			Button("call API") {
 
-//				viewModel.callAPI()
+				viewModel.callAPI()
 			}
 		}
 	}
@@ -118,7 +119,7 @@ final class FirstViewModel: ObservableObject {
 
 	@Published var coordinator: cenas
 
-//	private let network: RyukNetworkContract
+	private let network: RyukNetworkContract
 
 	deinit {
 		print("bye \(self)")
@@ -128,26 +129,26 @@ final class FirstViewModel: ObservableObject {
 		
 		self.coordinator = coordinator
 
-//		self.network = RyukNetwork(baseURL: URL(string: "https://jsonplaceholder.typicode.com")!)
+		self.network = RyukNetwork(baseURL: URL(string: "https://jsonplaceholder.typicode.com")!)
 
 		print("hi \(self)")
 	}
 
-//	func callAPI() {
-//
-//		Task {
-//			let resource = RyukNetworkResource(endpoint: "todos",
-//											   method: .post,
-//											   body: Todo(userId: 1, id: 1, title: "", completed: true))
-//
-//			do {
-//				let result = try await network.request(type: Empty.self, with: resource)
-//				print(result)
-//			} catch {
-//				print(error)
-//			}
-//		}
-//	}
+	func callAPI() {
+
+		Task {
+			let resource = RyukNetworkResource(endpoint: "todos",
+											   method: .post,
+											   body: Todo(userId: 1, id: 1, title: "", completed: true))
+
+			do {
+				let result = try await network.request(type: Empty.self, with: resource)
+				print(result)
+			} catch {
+				print(error)
+			}
+		}
+	}
 
 	func didTapButton() {
 		coordinator.goToDetailView()
