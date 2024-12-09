92468991bb66165779b7e8ed068727c7e6ecf1c9
joCs <jonybfc@gmail.com>
2024-12-09 19:13:44 +0000

    removed local dependency

--- TestCordin/TestCordin/Coordinator.swift
+++ TestCordin/TestCordin/Coordinator.swift
@@ -6,7 +6,6 @@
 //
 
 import SwiftUI
-import RyukNetwork
 
 protocol Coordinator: ObservableObject {
 
@@ -119,7 +118,7 @@ final class FirstViewModel: ObservableObject {
 
 	@Published var coordinator: cenas
 
-	private let network: RyukNetworkContract
+//	private let network: RyukNetworkContract
 
 	deinit {
 		print("bye \(self)")
@@ -129,25 +128,25 @@ final class FirstViewModel: ObservableObject {
 		
 		self.coordinator = coordinator
 
-		self.network = RyukNetwork(baseURL: URL(string: "https://jsonplaceholder.typicode.com")!)
+//		self.network = RyukNetwork(baseURL: URL(string: "https://jsonplaceholder.typicode.com")!)
 
 		print("hi \(self)")
 	}
 
 	func callAPI() {
 
-		Task {
-			let resource = RyukNetworkResource(endpoint: "todos",
-											   method: .post,
-											   body: Todo(userId: 1, id: 1, title: "", completed: true))
-
-			do {
-				let result = try await network.request(type: Empty.self, with: resource)
-				print(result)
-			} catch {
-				print(error)
-			}
-		}
+//		Task {
+//			let resource = RyukNetworkResource(endpoint: "todos",
+//											   method: .post,
+//											   body: Todo(userId: 1, id: 1, title: "", completed: true))
+//
+//			do {
+//				let result = try await network.request(type: Empty.self, with: resource)
+//				print(result)
+//			} catch {
+//				print(error)
+//			}
+//		}
 	}
 
 	func didTapButton() {
